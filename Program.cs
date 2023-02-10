887045fe09248584a247a2c17e52100e97d562b1
esweeney13 <122942750+esweeney13@users.noreply.github.com>
2023-02-10 12:32:08 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Loops_Application
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter an integer number");
+            int input = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Specify the series: Even or Odd");
+            string series = Console.ReadLine();
+
+            Console.WriteLine("You have selected the" + series + "The numbers between 0 and " + input + " are:");
+
+            if (series == "Even")
+            {
+                for (int i = 0; i < input; i = i + 2)
+                {
+                    Console.WriteLine(i + " is an even number");
+                }
+            }
+        }
+    }
+}
+
